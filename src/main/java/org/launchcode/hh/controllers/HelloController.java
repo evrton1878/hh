6b13e01edac646172b9fe6f9a42657b686aa0758
Boyan Vasilev <vasilev1878@gmail.com>
2021-02-19 12:28:48 -0600

    Controllers with query parameter

--- src/main/java/org/launchcode/hh/controllers/HelloController.java
+++ src/main/java/org/launchcode/hh/controllers/HelloController.java
@@ -2,17 +2,18 @@ package org.launchcode.hh.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-@GetMapping("hello")
-@ResponseBody
+//@GetMapping("hello")
+//@ResponseBody
 //handles request to /hello
-    public String hello(){
-        return "Hello Spring";
-    }
+   // public String hello(){
+      //  return "Hello Spring";
+    //}
     @GetMapping("goodbye")
     @ResponseBody
 //handles request to /hello
@@ -20,6 +21,12 @@ public class HelloController {
         return "goodbye Spring";
     }
 
+    //handeles request of the form /hello?name=everton
+@GetMapping("hello")
+    @ResponseBody
+    public String HelloWithQueryParam(@RequestParam String name){
+        return "hello " + name + "!";
+}
 }
 
 
